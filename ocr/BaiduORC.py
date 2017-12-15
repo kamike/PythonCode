29ec279bb62fde2da16a68381250a642e79398ea
wangtao <wangtao502@hotmail.com>
2017-12-15 18:27:11 +0800

    baidu res

--- ocr/BaiduORC.py
+++ ocr/BaiduORC.py
@@ -5,7 +5,7 @@ import configparser
 
 from aip import AipOcr
 import json
-import re
+
 
 # conf = configparser.ConfigParser()
 # file=os.getcwd()+"/Config.conf";
@@ -15,28 +15,23 @@ import re
 # print("section",session)
 # print(conf.get("baidu","a"))
 
+class BaiduORC(object):
+    def __init__(self):
+        # self.imgUrl = "http://pursuege.oss-cn-shenzhen.aliyuncs.com/img/timg.jpg";
+        self.client = AipOcr("10506105", "l6xFr9MFZrj1I6XkObWf7jqN", "3iDMlsadAXqy5CIOGIjW2pnr4KrHEey3 ")
+        # 定义参数变量
+        self.options = {
+            'detect_direction': 'true',
+            'language_type': 'CHN_ENG',
+        }
 
+    def decodeImgUrl(self, imgUrl):
+        jsonStr = self.client.basicGeneral(imgUrl, self.options)
 
-# imgUrl = "http://pursuege.oss-cn-shenzhen.aliyuncs.com/img/timg.jpg";
-#
-# client = AipOcr("10506105", "l6xFr9MFZrj1I6XkObWf7jqN", "3iDMlsadAXqy5CIOGIjW2pnr4KrHEey3 ")
-#
-# # 定义参数变量
-# options = {
-#     'detect_direction': 'true',
-#     'language_type': 'CHN_ENG',
-# }
-# resoult = client.basicGeneral(imgUrl, options)
+        # jsonStr="{'log_id': 1902239055352749612, 'direction': 0, 'words_result_num': 2, 'words_result': [{'words': '再不走'}, {'words': '就打死你'}]}"
+        jsonStr = str(jsonStr)
+        print(jsonStr)
+        jsonStr = jsonStr.replace("'", "\"")
 
-
-
-jsonStr="{'log_id': 1902239055352749612, 'direction': 0, 'words_result_num': 2, 'words_result': [{'words': '再不走'}, {'words': '就打死你'}]}"
-jsonStr=re.sub("'","\"",jsonStr)
-print(jsonStr)
-resoult=json.loads(jsonStr)
-
-print(resoult['log_id'])
-print(resoult['direction'])
-print(resoult['words_result_num'])
-print(resoult['words_result'])
-print(resoult['words_result'][0])
+        resoult = json.loads(jsonStr)
+        return resoult
--- sql/TestSql.py
+++ sql/TestSql.py
@@ -5,12 +5,12 @@ from OcrBean import OcrBean
 from SQLUtils import SQLUtils
 import hashlib
 
-# utils = SQLUtils()
-# fileBean = FileBean("http://baiodu.com","fileName",'a/dwaw','100',"md5111",'100','300',"baidu")
-# utils.addTableFile(fileBean)
-# orc=OcrBean("表情文字",0,"关键字","baidu","baidu.com")
-# utils.addTableOcr(orc)
-# utils.closedAll()
-md5file=open("G:/python_data/my_test/111/a.txt",'rb')
-md5=hashlib.md5(md5file.read()).hexdigest()
-print(md5)
+utils = SQLUtils()
+fileBean = FileBean("http://baiodu.com","fileName",'a/dwaw','100',"md5111",'100','300',"baidu")
+utils.addTableFile(fileBean)
+orc=OcrBean("表情文字",0,"关键字","baidu","baidu.com")
+utils.addTableOcr(orc)
+utils.closedAll()
+# md5file=open("G:/python_data/my_test/111/a.txt",'rb')
+# md5=hashlib.md5(md5file.read()).hexdigest()
+# print(md5)
